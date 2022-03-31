2a34b5c5b280fdb5b7896cc2b3284efe3ba650b1
mellonaut <rleeazw@outlook.com>
2022-03-31 14:20:47 -0400

    Teams and Removal
    Added Teams to the software section and made removing Boxstarter optional. For us, being able to update everyting with one command outweighs the risk having choco and boxstarter installed might pose.

--- newhire.ps1
+++ newhire.ps1
@@ -56,9 +56,10 @@ Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
 ###############################################################################
 choco feature enable -n allowGlobalConfirmation
 cinst -y microsoft-windows-terminal -y
-cinst -y office365business --params="/productid:O365ProPlusRetail" /exclude: 
+cinst -y office365business --params="/productid:O365ProPlusRetail" # /exclude: 
 cinst -y belarcadvisor
 cinst -y adobereader
+cinst -y microsoft-teams
 
 # Sysmon w/ custom configuration
 mkdir "C:\sysmon";
@@ -79,24 +80,26 @@ c:\sysmon\sysmon.exe -acceptEula -i c:\sysmon\sysmonconfig.xml
 
 
 
-###############################################################################
-# Remove Boxstarter
-###############################################################################
-# Remove the Chocolatey packages in a specific order!
-'Boxstarter.Azure', 'Boxstarter.TestRunner', 'Boxstarter.WindowsUpdate', 'Boxstarter',
-    'Boxstarter.HyperV', 'Boxstarter.Chocolatey', 'Boxstarter.Bootstrapper', 'Boxstarter.WinConfig', 'BoxStarter.Common' |
-    ForEach-Object { choco uninstall $_ -y }
 
-# Remove the Boxstarter data folder
-Remove-Item -Path (Join-Path -Path $env:ProgramData -ChildPath 'Boxstarter') -Recurse -Force
 
-# Remove Boxstarter from the path in both the current session and the system
-$env:PATH = ($env:PATH -split ';' | Where-Object { $_ -notlike '*Boxstarter*' }) -join ';'
-[Environment]::SetEnvironmentVariable('PATH', $env:PATH, 'Machine')
+# ###############################################################################
+# # Optionally Remove Boxstarter
+# ###############################################################################
+# # Remove the Chocolatey packages in a specific order!
+# 'Boxstarter.Azure', 'Boxstarter.TestRunner', 'Boxstarter.WindowsUpdate', 'Boxstarter',
+#     'Boxstarter.HyperV', 'Boxstarter.Chocolatey', 'Boxstarter.Bootstrapper', 'Boxstarter.WinConfig', 'BoxStarter.Common' |
+#     ForEach-Object { choco uninstall $_ -y }
 
-# Remove Boxstarter from the PSModulePath in both the current session and the system
-$env:PSModulePath = ($env:PSModulePath -split ';' | Where-Object { $_ -notlike '*Boxstarter*' }) -join ';'
-[Environment]::SetEnvironmentVariable('PSModulePath', $env:PSModulePath, 'Machine')
+# # Remove the Boxstarter data folder
+# Remove-Item -Path (Join-Path -Path $env:ProgramData -ChildPath 'Boxstarter') -Recurse -Force
+
+# # Remove Boxstarter from the path in both the current session and the system
+# $env:PATH = ($env:PATH -split ';' | Where-Object { $_ -notlike '*Boxstarter*' }) -join ';'
+# [Environment]::SetEnvironmentVariable('PATH', $env:PATH, 'Machine')
+
+# # Remove Boxstarter from the PSModulePath in both the current session and the system
+# $env:PSModulePath = ($env:PSModulePath -split ';' | Where-Object { $_ -notlike '*Boxstarter*' }) -join ';'
+# [Environment]::SetEnvironmentVariable('PSModulePath', $env:PSModulePath, 'Machine')
 
 
 ###############################################################################
